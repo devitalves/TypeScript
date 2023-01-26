99b6886c2db19d89b635a271cebe6bcf16e5e779
Ítalo Alves <101436057+devitalves@users.noreply.github.com>
2023-01-25 21:04:59 -0300

    Porque usar TypeScript?

---
+++ TypeScript - Evoluindo seu JavaScript/Porque usar TypeScript.js
@@ -0,0 +1,5 @@
+import { Negociacao } from './models/Negociacao.1';
+import { Negociacao } from './models/Negociacao.js';
+
+const negociacao = new Negociacao(new Date(),10,100);
+console.log(negociacao.volume);
